b04fdedb4d2bf2d8badaefb75fe50d8ed1140292
Joshua Entrop <entjos@users.noreply.github.com>
2023-04-12 11:03:11 +0200

    Added control option for calles to survfit

--- R/mean_no.R
+++ R/mean_no.R
@@ -17,6 +17,14 @@
 #'    for the competing event and one row for each risk episode of the
 #'    recurrent event
 #'
+#' @param re_control
+#'    An optional `list` with arguments passed to `survfit` when computing
+#'    risksets for the recurrent event.
+#'
+#' @param ce_control
+#'    An optional `list` with arguments passed to `survfit` when computing
+#'    risksets for the competing event.
+#'
 #' @return
 #'    A `data.frame` including the estimated number of events `expn`
 #'    at times `t`.
@@ -28,17 +36,25 @@
 mean_no <- function(formula,
                     re_indicator,
                     ce_indicator,
-                    data){
+                    data,
+                    re_control = list(),
+                    ce_control = list()){
 
-  # Define risk table for CB
-  risktab_re <- survival::survfit(formula,
-                                  data = data[data[[re_indicator]] == 1, ])
+  # Define risk table for CB ---------------------------------------------------
+  re_control$formula <- formula
+  re_control$data    <- data[data[[re_indicator]] == 1, ]
 
-  # Define risk table for competing event
-  risktab_ce <- survival::survfit(formula,
-                                  data = data[data[[ce_indicator]] == 1, ])
+  risktab_re <- do.call(survival::survfit,
+                        args = re_control)
 
-  # Combine models
+  # Define risk table for competing event --------------------------------------
+  ce_control$formula <- formula
+  ce_control$data    <- data[data[[ce_indicator]] == 1, ]
+
+  risktab_ce <- do.call(survival::survfit,
+                        args = ce_control)
+
+  # Combine models -------------------------------------------------------------
   re_df <- data.table::data.table(time = risktab_re$time,
                                   na   = risktab_re$n.event / risktab_re$n.risk)
 
@@ -54,16 +70,17 @@ mean_no <- function(formula,
   # Impute survival function
   if(is.na(comb_df$surv[[1]])) {
 
+    # Set survival to 1 at the beginning
     comb_df$surv[[1]] <- 1
 
   }
 
   comb_df[, surv := data.table::nafill(surv, type = "locf")]
 
-  # Estimate E[N(t)]
+  # Estimate E[N(t)] -----------------------------------------------------------
   comb_df[, expn := cumsum(surv * na)]
 
-  # Output
+  # Output ---------------------------------------------------------------------
   out <- data.frame(t = comb_df$time, expn = comb_df$expn)
 
   return(out)
