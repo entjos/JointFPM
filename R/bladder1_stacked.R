3ee3312b56a1046883bb95271a65a6604243dbd3
Joshua Entrop <entjos@users.noreply.github.com>
2023-10-12 13:54:58 +0200

    Added bladder1_stacked to the package

---
+++ R/bladder1_stacked.R
@@ -0,0 +1,17 @@
+#' Stacked version of the bladder1 dataset included in the survival package
+#'
+#' This dataset includes the bladder1 dataset included the survival package,
+#' which has been transformed into stacked format for use with `JointFPM`. The
+#' stacked datset includes one row per individual for the competing event and
+#' one rows per individual for each reoccurrence of bladder cancer.
+#'
+#' For more information please take a look at `?survival::bladder`.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name bladder1_stacked
+#' @usage
+#' bladder1_stacked
+#' data(bladder1_stacked)
+#' @format A data frame with 412 rows and 10 columns
+NULL
