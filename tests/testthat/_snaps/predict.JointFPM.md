4955c50ac2d1eba99b6d49d7235419b26d69b8d3
Joshua Entrop <entjos@users.noreply.github.com>
2023-10-12 13:53:30 +0200

    Add tests for predict.JointFPM

---
+++ tests/testthat/_snaps/predict.JointFPM.md
@@ -0,0 +1,13 @@
+# Mean number of events in bladder did not change
+
+    Code
+      bldr_model <- JointFPM(Surv(time = start, time2 = stop, event = event, type = "counting") ~
+        1, re_model = ~ pyridoxine + thiotepa, ce_model = ~ pyridoxine + thiotepa,
+      re_indicator = "re", ce_indicator = "ce", df_ce = 3, df_re = 3, cluster = "id",
+      data = bldr_stacked)
+      predict(bldr_model, newdata = data.frame(pyridoxine = 1, thiotepa = 0), t = c(
+        50), ci_fit = FALSE)
+    Output
+        stop      fit
+      1   50 2.430327
+
---
+++ tests/testthat/test-predict.JointFPM.R
@@ -0,0 +1,75 @@
+test_that("Mean number of events in bladder did not change",{
+  expect_snapshot({
+    bldr_model <- JointFPM(Surv(time  = start,
+                                time2 = stop,
+                                event = event,
+                                type  = 'counting') ~ 1,
+                           re_model = ~ pyridoxine + thiotepa,
+                           ce_model = ~ pyridoxine + thiotepa,
+                           re_indicator = "re",
+                           ce_indicator = "ce",
+                           df_ce = 3,
+                           df_re = 3,
+                           cluster  = "id",
+                           data     = bldr_stacked)
+
+    predict(bldr_model,
+            newdata = data.frame(pyridoxine = 1,
+                                 thiotepa   = 0),
+            t       =  c(50),
+            ci_fit  = FALSE)
+  })
+})
+
+test_that("Difference is correct",{
+  expect_equal({
+    bldr_model <- JointFPM(Surv(time  = start,
+                                time2 = stop,
+                                event = event,
+                                type  = 'counting') ~ 1,
+                           re_model = ~ pyridoxine + thiotepa,
+                           ce_model = ~ pyridoxine + thiotepa,
+                           re_indicator = "re",
+                           ce_indicator = "ce",
+                           df_ce = 3,
+                           df_re = 3,
+                           cluster  = "id",
+                           data     = bldr_stacked)
+
+    predict(bldr_model,
+            type = "diff",
+            newdata = data.frame(pyridoxine = 1,
+                                 thiotepa   = 0),
+            exposed = \(x) transform(x, thiotepa = 1),
+            t       =  c(50),
+            ci_fit  = FALSE)[["fit"]]
+  }, {
+
+    bldr_model <- JointFPM(Surv(time  = start,
+                                time2 = stop,
+                                event = event,
+                                type  = 'counting') ~ 1,
+                           re_model = ~ pyridoxine + thiotepa,
+                           ce_model = ~ pyridoxine + thiotepa,
+                           re_indicator = "re",
+                           ce_indicator = "ce",
+                           df_ce = 3,
+                           df_re = 3,
+                           cluster  = "id",
+                           data     = bldr_stacked)
+
+    e0 <- predict(bldr_model,
+                  newdata = data.frame(pyridoxine = 1,
+                                       thiotepa   = 0),
+                  t       =  c(50),
+                  ci_fit  = FALSE)
+
+    e1 <- predict(bldr_model,
+                  newdata = data.frame(pyridoxine = 1,
+                                       thiotepa   = 1),
+                  t       =  c(50),
+                  ci_fit  = FALSE)
+
+    e0$fit - e1$fit
+  })
+})
